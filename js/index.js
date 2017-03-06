6d77f73261f85888a828e8cd25cf82f4bd96e9f9
xuluxuanyi <xuqiang_java@163.com>
2017-03-06 21:17:53 +0800

    修改显示文字

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('Shape|Shifter|Type|to start|#rectangle|#countdown 3||');
+      S.UI.simulate('#rectangle|#countdown 10||XiaoTong|唯有|学习|青春|才能|远航||');
     }
 
     S.Drawing.loop(function () {
